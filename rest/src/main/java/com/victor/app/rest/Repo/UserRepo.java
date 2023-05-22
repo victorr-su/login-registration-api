8458ed1d5fb896992aa3430bfd6476f83e1deee0
Victor <su.victor21@gmail.com>
2023-05-22 19:12:21 -0400

    removed uneeded

--- rest/src/main/java/com/victor/app/rest/Repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.victor.app.rest.Repo;
-
-import com.victor.app.rest.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-}
